c3aee4eb143b705e7b181576c010b31afe237e07
mindy-liang <runst01@gmail.com>
2022-11-17 15:18:26 +0800

    api測試

--- 中選會_測試.R
+++ 中選會_測試.R
@@ -4,11 +4,19 @@ pacman::p_load(tidyverse,xml2, stringr, googledrive, googlesheets4,httr,readxl,j
 
 gs4_auth(email = "mindy.liang@thenewslens.com")
 
-#### import ####
-
 path <-"~/Library/Mobile Documents/com~apple~CloudDocs/Documents/九合一選舉-中選會"
 
-finalRaw <- fromJSON("~/Library/Mobile Documents/com~apple~CloudDocs/Documents/九合一選舉-中選會/final_T.json")
+#### import ####
+
+library(wget)
+
+download.file("https://download.2022dl.nat.gov.tw/running.json",destfile="running_T.json", method = "wget", extra="--no-check-certificate	--http-user=DL000005 --http-password=7JM3MAI")
+
+download.file("https://download.2022dl.nat.gov.tw/final.json",destfile="final_T.json", method = "wget", extra="--no-check-certificate	--http-user=DL000005 --http-password=7JM3MAI")
+
+running_T <- fromJSON("running_T.json")
+
+finalRaw <- running_T
 
 citycons <- rbind(finalRaw$T1,finalRaw$T2,finalRaw$T3)
 
@@ -31,14 +39,13 @@ citymayor_area <- citymayor %>%
   select(1:5) %>%
   mutate(areaCode = ifelse(is.na(areaCode), "00", areaCode),
          deptCode = ifelse(is.na(deptCode), "000", deptCode),
-         liCode = ifelse(is.na(liCode), "000", liCode),
-         tboxNo = ifelse(is.na(tboxNo), "0000", tboxNo))
+         liCode = ifelse(is.na(liCode), "000", liCode))
 
 village_area <- village %>%
-  select(1:5) %>%
+  select(1:6) %>%
   mutate(areaCode = ifelse(is.na(areaCode), "00", areaCode),
          deptCode = ifelse(is.na(deptCode), "000", deptCode),
          liCode = ifelse(is.na(liCode), "000", liCode),
-         tboxNo = ifelse(is.na(tboxNo), "0000", tboxNo))
+         tboxNo = ifelse(is.na(tboxNo),"0000",tboxNo))
 
 
